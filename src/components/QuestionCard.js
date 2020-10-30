edbd2a6db16efa4e942541408df099365dc6be66
rainswerld <m.rainrobinson@gmail.com>
2020-10-30 12:04:12 -0600

    Begin data implementation in the card

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,18 +1,22 @@
 import React from 'react'
-// import quiz from '../data/Apprentice_TandemFor400_Data.json'
+import quiz from '../data/Apprentice_TandemFor400_Data.json'
 import { Card, ListGroup } from 'react-bootstrap'
 import './QuestionCard.css'
 
 function QuestionCard() {
-  return (
-    <Card className="card-style">
-      <Card.Title className="card-title">map questions here</Card.Title>
+  const questions = quiz.map(data => (
+    <Card>
+      <Card.Title className="card-title">{data.question}</Card.Title>
       <Card.Body>
         <ListGroup>
-          <ListGroup.Item>map answer choices here</ListGroup.Item>
+          <ListGroup.Item>{data.correct}</ListGroup.Item>
         </ListGroup>
       </Card.Body>
     </Card>
+  ))
+
+  return (
+    <div>{questions}</div>
   )
 }
 
